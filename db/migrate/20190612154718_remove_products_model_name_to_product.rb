a9d6e5cc21ea3edd62ecf2b27ea13a67c4784abb
Hongin Yun <hongin.yun@kellogg.northwestern.edu>
2019-06-14 16:21:32 +0000

    Final project submission 2

--- db/migrate/20190612154718_remove_products_model_name_to_product.rb
+++ db/migrate/20190612154718_remove_products_model_name_to_product.rb
@@ -1,6 +1,6 @@
 class RemoveProductsModelNameToProduct < ActiveRecord::Migration[5.1]
   def change
-    remove_column :products, :model_name
+    #remove_column :products, :model_name
     add_column :products, :modelname, :string
   end
 end
