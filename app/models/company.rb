857558a97b1d2c36dab2541f2c83c2bfd3b40046
honginyun <honginyun132@gmail.com>
2019-11-20 04:27:01 +0000

    Company find_company_name_by_id method modified

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,8 +13,12 @@ class Company < ApplicationRecord
     has_many :company_products, :dependent => :nullify
     
     def self.find_company_name_by_id(company_id)
-        company = Company.where({ :id => company_id }).first
-        return company.name 
+        @company = Company.where({ :id => company_id }).first
+        if @company.present?
+            return @company.name 
+        else 
+            return "Company name"
+        end
     end
 
 end
